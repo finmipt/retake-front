730f1bec157fa2eaaf3b8141402093c41fbf699e
Ilia Bogatyrev <finmipt@gmail.com>
2024-06-04 18:40:47 +0300

    Fixed Journal.vue

--- src/components/duty/Journal.vue
+++ src/components/duty/Journal.vue
@@ -41,7 +41,7 @@ const fetchRegistrationData = async () => {
 const fetchEvent = async () => {
   try {
     const jwt = getCookie('jwt');
-    const response = await axios.get(`${BACK_END}/event/get_event/${eventId.value}`, {
+    const response = await axios.get(`${BACK_END}/event/${eventId.value}`, {
       headers: {
         'Authorization': `${jwt}`
       }
